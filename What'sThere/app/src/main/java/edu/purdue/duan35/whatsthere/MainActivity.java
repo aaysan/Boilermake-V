1efd1cba56365d9459cc1d730a08045d2ea941ca
Alp Aysan <alpaysan@pal-nat186-77-176.itap.purdue.edu>
2017-09-30 18:26:07 -0400

    a little hackson touch

--- What'sThere/app/src/main/java/edu/purdue/duan35/whatsthere/MainActivity.java
+++ What'sThere/app/src/main/java/edu/purdue/duan35/whatsthere/MainActivity.java
@@ -5,15 +5,21 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
+import android.view.Window;
+import android.widget.TextView;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 
     public void add (View view) {
         goToUrl("http://google.com");
