73f4e61a82e589af43e13d72efd78f2804ef8cfe
Ajay1997 <ajayGkumar97@gmail.com>
2017-09-30 12:10:01 -0400

    Clarifai food image recognition calls fixed

--- foodapi_Clarifai.py
+++ foodapi_Clarifai.py
@@ -7,15 +7,18 @@ def fooditem(new_image):
     model = app.models.get('food-items-v1.0')
     image = ClImage(file_obj=open(new_image, 'rb'))
     r =  model.predict([image])
+    food_items = dict()
     #pprint.pprint(model.predict([image]))
-    food_items = {r['outputs'][0]['data']['concepts'][0]['name']+ \
-                 r['outputs'][0]['data']['concepts'][1]['name']+ \
-                 r['outputs'][0]['data']['concepts'][2]['name']+ \
-                 r['outputs'][0]['data']['concepts'][3]['name']+ \
-                 r['outputs'][0]['data']['concepts'][4]['name']}
+    food_items = [r['outputs'][0]['data']['concepts'][0]['name'], \
+                 r['outputs'][0]['data']['concepts'][1]['name'], \
+                 r['outputs'][0]['data']['concepts'][2]['name'], \
+                 r['outputs'][0]['data']['concepts'][3]['name'], \
+                 r['outputs'][0]['data']['concepts'][4]['name']]
     return food_items
     #print r['outputs'][0]['data']['concepts'][0]['name'] #Option 1
     #print r['outputs'][0]['data']['concepts'][1]['name'] #Option 2
     #print r['outputs'][0]['data']['concepts'][2]['name'] #Option 3
     #print r['outputs'][0]['data']['concepts'][3]['name'] #Option 4
     #print r['outputs'][0]['data']['concepts'][4]['name'] #Option 5
+
+print fooditem("test.jpg")
