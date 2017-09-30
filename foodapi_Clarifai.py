3e191c030cf567d6e9336181a65a4b88dc631bb3
Ajay1997 <ajayGkumar97@gmail.com>
2017-09-30 11:18:54 -0400

    Clarifai food image recognition complete

---
+++ foodapi_Clarifai.py
@@ -0,0 +1,21 @@
+from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
+import pprint
+app = ClarifaiApp(api_key='e5af645934f34cee8ec140028c1fff12')
+
+def fooditem(image):
+    model = app.models.get('food-items-v1.0')
+    image = ClImage(file_obj=open('test.jpg', 'rb'))
+    r =  model.predict([image])
+    #pprint.pprint(model.predict([image]))
+    food_items = {r['outputs'][0]['data']['concepts'][0]['name']+ \
+                 r['outputs'][0]['data']['concepts'][1]['name']+ \
+                 r['outputs'][0]['data']['concepts'][2]['name']+ \
+                 r['outputs'][0]['data']['concepts'][3]['name']+ \
+                 r['outputs'][0]['data']['concepts'][4]['name']}
+    return food_items
+    #print r['outputs'][0]['data']['concepts'][0]['name'] #Option 1
+    #print r['outputs'][0]['data']['concepts'][1]['name'] #Option 2
+    #print r['outputs'][0]['data']['concepts'][2]['name'] #Option 3
+    #print r['outputs'][0]['data']['concepts'][3]['name'] #Option 4
+    #print r['outputs'][0]['data']['concepts'][4]['name'] #Option 5
