4450d8b4f937d1a1b9a3a97111e476ddddf9aa60
Ajay1997 <ajayGkumar97@gmail.com>
2017-09-30 19:00:17 -0400

    Camera function changed ,now needs button press

--- camera.py
+++ camera.py
@@ -2,15 +2,26 @@ import cv2
 
 camera = cv2.VideoCapture(0)
 def capture_image():
-	# read is the easiest way to get a full image out of a VideoCapture object.
-	retval, im = camera.read()
-	return im
+ # read is the easiest way to get a full image out of a VideoCapture object.
+ retval, im = camera.read()
+ 
+ return im
+c = True
+  
+    
 
-def main():
-	for i in range(0,50):
-	    temp = capture_image()
-	    # Capture frame-by-frame
-	capture = capture_image() 
-	file1 = "barcode_01.jpg"
-	cv2.imwrite(file1, capture)
-	# del(camera)
+while c == True:
+    ret_val, img = camera.read()
+        
+    cv2.imshow('my webcam', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        c = False
+        break
+temp = capture_image()
+        
+cv2.destroyAllWindows()
+    # Capture frame-by-frame
+capture = capture_image() 
+file1 = "pic1.jpeg"
+cv2.imwrite(file1, capture)
+del(camera)
